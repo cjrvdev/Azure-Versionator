312eafcb644629bd355791d0414f981c968e4d7c
Carlos Rodriguez Vega <c.a.rodriguez.vega@avanade.com>
2026-06-26 10:11:46 +0200

    Bump package version to 1.0.1 in build.gradle.kts

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -22,7 +22,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "dev.cjrv.azureversionator"
-            packageVersion = "1.0.0"
+            packageVersion = "1.0.1"
             macOS{
                 iconFile.set(rootProject.file("shared/src/commonMain/composeResources/drawable/appicon_macos.icns"))
             }
